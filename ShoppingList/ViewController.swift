80375758fcd5e330eedc612a7d3d1c613ce35b26
bclaringbold <student@students-mac.student.georgianc.on.ca>
2016-02-26 11:03:35 -0500

    adding additional code documentation

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -2,8 +2,12 @@
 //  ViewController.swift
 //  ShoppingList
 //
-//  Created by student on 2016-02-26.
-//  Copyright © 2016 student. All rights reserved.
+//  Basic UI functionality for a shopping list app
+//  
+//
+//
+//  Created by Brad Claringbold on 2016-02-26.
+//  Copyright © 2016 Brad Claringbold. All rights reserved.
 //
 
 import UIKit
@@ -14,6 +18,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     
+        // Stepper Options
+        // wraps - Enables value to go from max value back to 0 and from 0 to max value
+        // autorepeat - allows data to increase while button is held
+        // maximum Value - max vlue stepper will go to
+        
         item1Stepper.wraps = false
         item1Stepper.autorepeat = true
         item1Stepper.maximumValue = 9
@@ -45,6 +54,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    //Add in each text item for the shopping list items
     @IBOutlet weak var shoppingListName: UITextField!
     @IBOutlet weak var item1Text: UITextField!
     @IBOutlet weak var item2Text: UITextField!
@@ -54,6 +65,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var item6Text: UITextField!
     
 
+    //Stepper controls
     @IBOutlet weak var item1Label: UILabel!
     @IBOutlet weak var item1Stepper: UIStepper!
     @IBAction func item1ValueChange(sender: UIStepper) {
@@ -92,6 +104,8 @@ class ViewController: UIViewController {
         item6Label.text = Int(sender.value).description
     }
     
+    
+    //cancel button
     @IBAction func buttoncancel(sender: UIButton) {
         
         //clear stepper values and labels
@@ -108,7 +122,7 @@ class ViewController: UIViewController {
         item6Stepper.value=0;
         item6Label.text = "0";
         
-        //clear item txt boxes
+        //clear item text boxes
         item1Text.text = ""
         item2Text.text = ""
         item3Text.text = ""
